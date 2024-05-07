2c6409f3f6d8511483c012b264734dce697e686c
kennyChukwuebuka <kenkinado@gmail>
2024-05-07 11:14:59 +0100

    method to validate utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -33,12 +33,11 @@ def validUTF8(data):
                 num_bytes = 2
             elif byte >> 3 == 0b11110:
                 num_bytes = 3
-            elif byte >> 7 == 0b1:
+            elif byte >> 7:
                 return False
         else:
-            if byte >> 6 == 0b10:
-                num_bytes -= 1
-            else:
+            if byte >> 6 != 0b10:
                 return False
+            num_bytes -= 1
 
     return num_bytes == 0
