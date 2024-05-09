fe23076ef740fd2ba4a2ebde99166b057a85b6aa
kennyChukwuebuka <kenkinado@gmail>
2024-05-09 11:22:03 +0100

    hotfix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,36 +13,26 @@ validate_utf8 module
 
 
 def validUTF8(data):
+    """Determines if a given data set represents
+     a valid UTF-8 encoding or not
     """
-    Determines if a given data set is a valid UTF-8 encoding
-
-    Args:
-        data (list): A list of integers
-
-    Returns:
-        If a valid UTF-8 encoding is detected
-        return True, otherwise return False
-    """
-    num_bytes = 0
-
-    for byte in data:
-        if num_bytes == 0:
-            # Check if the byte represents the start of a character
-            if (byte >> 7) == 0:  # 1-byte character
-                num_bytes = 0
-            elif (byte >> 5) == 0b110:  # 2-byte character
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:  # 3-byte character
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 4-byte character
-                num_bytes = 3
+    count = 0
+    for d in data:
+        if count == 0:
+            if d & 128 == 0:
+                count = 0
+            elif d & 224 == 192:
+                count = 1
+            elif d & 240 == 224:
+                count = 2
+            elif d & 248 == 240:
+                count = 3
             else:
-                return False  # Invalid start of a character
+                return False
         else:
-            # Check if the byte is a continuation byte
-            if (byte >> 6) != 0b10:
-                return False  # Invalid continuation byte
-            num_bytes -= 1
-
-    # If there are remaining bytes required, return False
-    return num_bytes == 0
+            if d & 192 != 128:
+                return False
+            count -= 1
+    if count == 0:
+        return True
+    return False
