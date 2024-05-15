425224527fafd7baa5defa493907b610a54b0c5a
kennyChukwuebuka <kenkinado@gmail>
2024-05-15 15:34:02 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,36 +6,55 @@
 import sys
 
 
-def nqueens(n):
+def is_safe(board, row, col):
     """
-    nqeen
+        is safe methos
     """
-    def solve(queens, xy_dif, xy_sum):
-        p = len(queens)
-        if p == n:
-            result.append(queens)
-            return None
-        for q in range(n):
-            if q not in queens and p - q not in xy_dif and p + q not in xy_sum:
-                solve(queens + [q], xy_dif + [p - q], xy_sum + [p + q])
-    result = []
-    solve([], [], [])
-    return result
+    for i in range(row):
+        if board[i] == col:
+            return False
+
+        # Check diagonals
+        if abs(i - row) == abs(board[i] - col):
+            return False
+    return True
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N", file=sys.stderr)
-    sys.exit(1)
+def solve_n_queens(n):
+    """
+        solving the nqueen
+    """
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be a number", file=sys.stderr)
-    sys.exit(1)
+    board = [-1] * n
+    solutions = []
 
-if n < 4:
-    print("N must be at least 4", file=sys.stderr)
-    sys.exit(1)
+    def backtrack(row):
+        if row == n:
+            solutions.append([[i, board[i]] for i in range(n)])
+        else:
+            for col in range(n):
+                if is_safe(board, row, col):
+                    board[row] = col
+                    backtrack(row + 1)
+                    board[row] = -1
 
-for solution in nqueens(n):
-    print([[i, solution.index(i)] for i in solution])
+    backtrack(0)
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solve_n_queens(N)
