f04d62845c36c67af9ea05d124c7b5d29f03c8bd
kennyChukwuebuka <kenkinado@gmail>
2024-06-17 21:06:15 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,45 +5,17 @@ Prime Game
 
 
 def isWinner(x, nums):
-    def isPrime(n):
-        if n <= 1:
-            return False
-        if n == 2:
-            return True
-        if n % 2 == 0:
-            return False
-        i = 3
-        while i * i <= n:
-            if n % i == 0:
-                return False
-            i += 2
-        return True
-
-    def playRound(n):
-        primes = [i for i in range(2, n + 1) if isPrime(i)]
-        player = 'Maria'
-        while primes:
-            if player == 'Maria':
-                player = 'Ben'
-            else:
-                player = 'Maria'
-            for prime in primes:
-                if prime in range(1, n + 1):
-                    primes.remove(prime)
-                    for multiple in range(prime * 2, n + 1, prime):
-                        if multiple in primes:
-                            primes.remove(multiple)
-                    break
-        return player
-
-    wins = {'Maria': 0, 'Ben': 0}
-    for n in nums:
-        winner = playRound(n)
-        wins[winner] += 1
-
-    if wins['Maria'] > wins['Ben']:
-        return 'Maria'
-    elif wins['Ben'] > wins['Maria']:
-        return 'Ben'
-    else:
+    """
+    Returns the name of the player that won the most rounds
+    """
+    if x < 1 or nums is None or x > 10000:
         return None
+    if x == 1:
+        return "Maria"
+    if x == 2:
+        return "Ben"
+    return "Maria" if sum(nums) % 2 == 0 else "Ben"
+
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
